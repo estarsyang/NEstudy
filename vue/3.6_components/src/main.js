7b15957701509d0d539101ef284222bfd94deb61
estarsyang <455089957@qq.com>
2021-01-27 16:20:29 +0800

    feat: add formValidate initVersion

--- vue/3.6_components/src/main.js
+++ vue/3.6_components/src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import UiInstall from "./utils/ui-install";
+import "@/assets/style/index.scss";
 
 Vue.config.productionTip = false;
+Vue.use(UiInstall);
 
 new Vue({
   render: h => h(App)
 }).$mount("#app");
-
-
